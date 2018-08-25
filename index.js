a834957b74aa8598b96b9301057d5c939a3d79a4
Factman <factman60@gmail.com>
2018-08-25 17:14:12 +0100

    Eslint Implimentation.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /*
- * @Copyright: 2018 
- * 
- * @Author: Mohammed Odunayo 
- * @Email: factman60@gmail.com 
- * @Date: 2018-08-24 00:16:08 
+ * @Copyright: 2018
+ *
+ * @Author: Mohammed Odunayo
+ * @Email: factman60@gmail.com
+ * @Date: 2018-08-24 00:16:08
  * @Last Modified by: Mohammed Odunayo
- * @Last Modified time: 2018-08-24 01:12:51
+ * @Last Modified time: 2018-08-25 17:08:07
  */
 
 /**
@@ -15,9 +15,9 @@
  * @returns {string} string
  */
 module.exports = {
-  capitalize: function (str) {
-    const arr = str.split("");
+  capitalize(str) {
+    const arr = str.split('');
     arr[0] = arr[0].toUpperCase();
-    return arr.join("");
+    return arr.join('');
   },
 };
